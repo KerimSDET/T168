467f332e9a22768ec477e6afc883ab9cdbe76a56
KerimSDET <krmtrpn@gmail.com>
2026-01-19 23:56:10 +0300

    creating Nested Maps

--- src/K36_maps/D05_nestedMapOlusturma/C01_FarkliDataTurleriKullanma.java
+++ src/K36_maps/D05_nestedMapOlusturma/C01_FarkliDataTurleriKullanma.java
@@ -1,4 +1,64 @@
 package K36_maps.D05_nestedMapOlusturma;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class C01_FarkliDataTurleriKullanma {
+
+    public static void main(String[] args) {
+
+        List<Integer> sayilar = new ArrayList<>();
+
+//        sayilar.add(12);
+//        //sayilar.add("Kerim"); int olan liste başka tür eleman eklenemez
+
+        List<Object> herseyListe = new ArrayList<>(); //Object herşeyin parenti oldugu için artık herşeyi liste ekleriz
+
+        herseyListe.add(12);
+        herseyListe.add("Kerem");
+        herseyListe.add('k');
+        herseyListe.add(true);
+
+        Integer[] arr = {4,5,6,7,8};
+        herseyListe.add(arr);
+        List<String> isimler = new ArrayList<>(Arrays.asList("Kerem","Taner","Tirpan"));
+
+        herseyListe.add(isimler);
+
+        /*
+        sayilar listesi deklare edilirken Integer'lardan olusagi soylendigi icin
+        Integer olmayan hic bir datayi ekleyemedik.
+         */
+
+        /*
+        bir listeye herseyi eklemek KOLAYLIK gibi gorunse de elemanlara ulasma ve
+        kullanma acisindan zorluk olusturabilir
+         */
+
+        System.out.println(herseyListe);
+        //[12, Kerem, k, true, [Ljava.lang.Integer;@3feba861, [Kerem, Taner, Tirpan]]
+        //Arrayler toString methoduyla cagirilabildigi icin bu sekilde dogru veri alinamadi
+
+        System.out.println((Integer)herseyListe.get(0)*2); //0 indexteki elemanı çarp dedik , cast edersek sorun kalmaz 24 verdi
+
+        //System.out.println((Integer)herseyListe.get(1)*2); //hata verir. java.lang.ClassCastException çünkü stringi çarpak istedik
+
+        //1. index'deki String'in  length'ini yazdirin.
+
+        System.out.println(((String) herseyListe.get(1)).length()); //ilk elemanı yazdırmak istedigimizde o elemaiinin turunu vermeden islem yapmaz
+        //cunku kendisi object tir.
+
+        //4. indexteki array'i yazdıralım
+
+        System.out.println(Arrays.toString((Integer[]) herseyListe.get(4))); //[4, 5, 6, 7, 8]
+
+        //4. indexteki arrayin length ini yazdıralim
+
+        System.out.println(((Integer[]) herseyListe.get(4)).length); //5
+
+
+    }
 }
--- src/K36_maps/D05_nestedMapOlusturma/C02_RezervasyonMapiOlusturma.java
+++ src/K36_maps/D05_nestedMapOlusturma/C02_RezervasyonMapiOlusturma.java
@@ -1,4 +1,104 @@
 package K36_maps.D05_nestedMapOlusturma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class C02_RezervasyonMapiOlusturma {
+
+    public static void main(String[] args) {
+
+
+         /*
+
+           asagidaki rezervasyon'u bir map olarak olusturun
+
+
+           {
+                "firstname" : "Ahmet",
+                "lastname" : "Bulut",
+                "totalprice" : 500,
+                "depositpaid" : false,
+                "bookingdates" : {
+                       "checkin" : "2024-07-21",
+                       "checkout" : "2024-08-10"
+                          },
+                "additionalneeds" : "wi-fi"
+            }
+
+         */
+
+
+        /*
+        dikkate alacağımız hususlar:
+        1-hangi bilgileri tutacagiz?
+        2-Key ve Value'nun data türü ne olacak?
+         */
+
+
+        Map<String, String> bookingDatesValueMap = new HashMap<>(); //önce nested Mapi düzenledik. sonra yerine tanımladık
+        bookingDatesValueMap.put("checkin", "2024-07-21");
+        bookingDatesValueMap.put("checkout", "2024-08-10");
+
+
+        Map<String, Object> reservationMap = new HashMap<>();
+
+        reservationMap.put("firstname", "Ahmet");
+        reservationMap.put("lastname", "Bulut");
+        reservationMap.put("totalprice",500);
+        reservationMap.put("depositpaid",false);
+        reservationMap.put("bookingdates",bookingDatesValueMap); //nested Map yerine konuldu.
+        reservationMap.put("additionalneeds","wi-fi");
+
+        System.out.println(reservationMap);
+        /*
+        {  map olması sebebiyle sıralama ddeğişkendir.
+
+         firstname=Ahmet,
+         additionalneeds=wi-fi,
+         bookingdates={
+                      checkin=2024-07-21,
+                      checkout=2024-08-10
+                      },
+         totalprice=500,
+         depositpaid=false,
+         lastname=Bulut
+         }
+         */
+        System.out.println(reservationMap.get("firstname")); //Ahmet
+        System.out.println(reservationMap.get("lastname")); //Bulut
+        System.out.println(reservationMap.get("totalprice")); //5000
+        System.out.println(reservationMap.get("depositpaid")); //false
+        System.out.println(reservationMap.get("bookingdates")); //{checkin=2024-07-21, checkout=2024-08-10} nested farklı yola devam et. yazdir
+
+        System.out.println(((Map<String, String>) reservationMap.get("bookingdates")).get("checkin"));
+        System.out.println(((Map<String, String>) reservationMap.get("bookingdates")).get("checkout"));
+
+        System.out.println(reservationMap.get("additionalneeds"));//wifi
+
+
+        /*
+            {
+              firstname=Ahmet,
+              additionalneeds=wi-fi,
+              bookingdates=
+                          {
+                            checkin=2024-07-21,
+                            checkout=2024-08-10
+                            },
+              totalprice=500,
+              depositpaid=false,
+              lastname=Bulut
+              }
+         */
+
+
+        // totalprice 400'den dusuk ise "super", 400 veya daha fazla ise "pahali" yazdirin
+
+        Integer totalPriceDegeri = (Integer) reservationMap.get("totalprice");
+        if (totalPriceDegeri <400){
+            System.out.println("Super");
+        } else {
+            System.out.println("Pahali");
+        }
+    }
 }
