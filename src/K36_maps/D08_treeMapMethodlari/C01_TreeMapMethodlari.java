26d9b5315a8d27b2b19bb483771e724bc61e44c2
KerimSDET <krmtrpn@gmail.com>
2026-01-22 01:18:31 +0300

    tree maps methods

--- src/K36_maps/D08_treeMapMethodlari/C01_TreeMapMethodlari.java
+++ src/K36_maps/D08_treeMapMethodlari/C01_TreeMapMethodlari.java
@@ -1,4 +1,57 @@
 package K36_maps.D08_treeMapMethodlari;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 public class C01_TreeMapMethodlari {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A",22);
+        map1.put("Z",33);
+        map1.put("B",44);
+        map1.put("Y",55);
+        map1.put("K",66);
+
+        System.out.println(map1); //{A=22, B=33, C=44, D=55, E=66}
+
+        map1.put("T",77);
+        map1.put("L",88);
+        System.out.println(map1); //{A=22, B=44, T=77, Y=55, Z=33, K=66, L=88} siralama garantisi yok
+
+
+        Map<String, Integer> map2 = new TreeMap<>();
+        map2.put("A",22);
+        map2.put("Z",33);
+        map2.put("B",44);
+        map2.put("Y",55);
+        map2.put("K",66);
+
+        System.out.println(map2); //{A=22, B=33, C=44, D=55, E=66}
+
+        map2.put("T",77);
+        map2.put("L",88);
+        System.out.println(map2); //{A=22, B=44, K=66, L=88, T=77, Y=55, Z=33} siralama garantisi var
+
+        /*
+        Constructor TreeMap secilmis olsa da Data turu olarak Map secildigi icin
+        map2 BUTUN MAP'lerle ortak method isimlerine sahiptir.
+
+        TreeMap class'ina sonradan eklenen ve sadece TreeMap'lerin sahip oldugu methodlar
+        map2'de gorunmez
+         */
+
+        TreeMap <String, Integer> map3 = new TreeMap<>();
+        map3.put("A",22);
+        map3.put("Z",33);
+
+        System.out.println(map3); //{A=22, Z=33}
+
+        /*
+        Constructor ve data turu TreeMap secildigi icin TreeMap'e eklenen ve parent'i olan Map'de olmayan
+        pekcok ozelligi kullanabiliriz
+         */
+
+
+    }
 }
--- src/K36_maps/D08_treeMapMethodlari/C02_TreeMapMethodlari.java
+++ src/K36_maps/D08_treeMapMethodlari/C02_TreeMapMethodlari.java
@@ -1,4 +1,95 @@
 package K36_maps.D08_treeMapMethodlari;
 
+
+import java.util.TreeMap;
+
 public class C02_TreeMapMethodlari {
+
+
+    public static void main(String[] args) {
+
+
+        TreeMap<String,Integer> harflerMap = new TreeMap<>();
+        harflerMap.put("D",11);
+        harflerMap.put("P",22);
+        harflerMap.put("S",33);
+        harflerMap.put("F",14);
+        harflerMap.put("O",5);
+
+        TreeMap<String,Integer> harflerMap2 = new TreeMap<>();
+
+        //TreeMap DOGAL SIRALI oldugunda bir elemanin sirada onundakiler veya arkasindakilar ile ilgili
+        //pek cok method vardir.
+
+        System.out.println(harflerMap); //{D=11, F=14, O=5, P=22, S=33}
+        System.out.println(harflerMap2); //{}
+
+
+        // 1- headMap verilen key'den basa kadar olan tum map'i verir.
+
+        System.out.println(harflerMap.headMap("R")); // harf olmasa da sirasinda bakar
+                                                    // olsa nerde olurdu tespit eder oradan basa dogru yazdiririn
+
+        System.out.println(harflerMap.headMap("r")); //char sirasina bakiyor..!!
+
+
+        System.out.println(harflerMap.headMap("P",true)); //eğer 2. parametre olarak true girilirse
+        //girilen key bitis keyi olmasina ragmen dahil eder
+
+        // 2- TailMap : girilen key dahil sonrasindaki entry'leri verir
+
+        System.out.println(harflerMap.tailMap("F")); //baslangic oldugu icin dahil olur
+
+        System.out.println(harflerMap.tailMap("K")); //baslangic oldugu icin olsa nerede olur bakar dahil olur
+
+        System.out.println(harflerMap.tailMap("C",false));//C dahil değil demek
+
+
+        // 3- lowerkey : verilen key'den kucuk olan keylerin en buyugu diger bir deyisle
+        // verilen key'den bir onceki key
+        //flooeKey : verilen key'den kucuk veya esit olan tum key'ler icinde en buyugu verir.
+        //diger bir deyisle verilen key'i asagi dogru yuvarlar
+
+
+        System.out.println(harflerMap.lowerKey("R")); //P
+        System.out.println(harflerMap.lowerKey("G")); // F
+
+
+        System.out.println(harflerMap.floorKey("S")); // S
+        System.out.println(harflerMap.floorKey("R")); // P
+
+        // 4- higherKey : verilen key'den buyuk olan key'ler icerisinde en kucuk olani verir.
+        //ceilingKey : verilen key'den buyuk esit olan keyler icindeki en kucuk olani verir
+
+
+
+        System.out.println(harflerMap.higherKey("R")); //S
+        System.out.println(harflerMap.higherKey("G")); //O
+
+
+        System.out.println(harflerMap.ceilingKey("S")); // S
+        System.out.println(harflerMap.ceilingKey("R")); // S
+
+
+        // 5- pollFirstEntry() ilk entry i siler ve bize getirir.
+
+
+        System.out.println(harflerMap.pollFirstEntry()); //D=11
+
+        System.out.println(harflerMap.pollLastEntry()); //S=33
+
+        System.out.println(harflerMap2.pollFirstEntry()); //null
+
+        System.out.println(harflerMap2.pollLastEntry()); //null
+
+        System.out.println(harflerMap.descendingMap()); //{P=22, O=5, F=14}
+
+        System.out.println(harflerMap.subMap("G", "P")); //{O=5}
+
+        System.out.println(harflerMap.subMap("B", false, "T", true)); //{F=14, O=5, P=22}
+
+
+    }
+
+
 }
